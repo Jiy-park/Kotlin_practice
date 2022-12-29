48e3a0a9ad3cedee2e6dbf2b6fa0de7d0f3c8858
Jiy-park <jiyong3954@gmail.com>
2022-12-29 15:33:14 +0900

    안드로이드(코틀린) 기본 연습 2

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import com.example.kotlin_practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.tv1.text = "Hello android with Kotlin!"
