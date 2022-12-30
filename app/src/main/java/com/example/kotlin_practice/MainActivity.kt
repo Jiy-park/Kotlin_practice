0937d42e7aa0f2f7b8cfb091ad0e5a455803b71c
Jiy-park <jiyong3954@gmail.com>
2022-12-30 14:38:42 +0900

    안드로이드(코틀린) 기본 연습 4 프로그레스바 스레드 활용

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.CompoundButton
 import com.example.kotlin_practice.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -40,9 +41,15 @@ class MainActivity : AppCompatActivity() {
                 R.id.rb3 -> Log.d("radio", "btn3_checked")
             }
         }
-
         binding.ch1.setOnCheckedChangeListener(listener)
         binding.ch2.setOnCheckedChangeListener(listener)
+//        Thread.sleep(3000) 메인 스레드 (앱 상에 그림을 그리는 스레드)를 잠시 멈춤
+        thread(start=true){ // 서브 스레드를 생성 (백그라운드 스레드) 후 해당 스레드를 잠시 멈춤
+            Thread.sleep(3000)
+            runOnUiThread{ // ui 관련 코드는 메인 스레드에서만 실행 되어야 함
+                showProgressBar(false)
+            }
+        }
     }
     fun showProgressBar(show:Boolean){
         if(show == true) { binding.pb1.visibility = View.VISIBLE }
