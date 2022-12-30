c50cfa158291a626f1a49b1b2928c63b3d28164a
Jiy-park <jiyong3954@gmail.com>
2022-12-30 15:07:43 +0900

    안드로이드(코틀린) 기본 연습 5 시크바 레이팅 바 기타

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -5,54 +5,33 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.CompoundButton
+import android.widget.SeekBar
 import com.example.kotlin_practice.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    val listener by lazy {
-        CompoundButton.OnCheckedChangeListener { button, isChecked ->
-            if(isChecked){
-                when(button.id){
-                    R.id.ch1 -> Log.d("check", "box1_checked")
-                    R.id.ch2 -> Log.d("check", "box2_checked")
-                    R.id.ch3 -> Log.d("check", "box2_checked")
-                }
-            }
-            else{
-                when(button.id){
-                    R.id.ch1 -> Log.d("check", "box1_not_checked")
-                    R.id.ch2 -> Log.d("check", "box2_not_checked")
-                    R.id.ch3 -> Log.d("check", "box3_not_checked")
-                }
-            }
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        binding.tv1.text = "Hello android with Kotlin!"
-
-        binding.group.setOnCheckedChangeListener { radioGroup, checkedid ->
-            when(checkedid){
-                R.id.rb1 -> Log.d("radio", "btn1_checked")
-                R.id.rb2 -> Log.d("radio", "btn2_checked")
-                R.id.rb3 -> Log.d("radio", "btn3_checked")
+        binding.seek1.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.tv1.text = "$progress"
             }
-        }
-        binding.ch1.setOnCheckedChangeListener(listener)
-        binding.ch2.setOnCheckedChangeListener(listener)
-//        Thread.sleep(3000) 메인 스레드 (앱 상에 그림을 그리는 스레드)를 잠시 멈춤
-        thread(start=true){ // 서브 스레드를 생성 (백그라운드 스레드) 후 해당 스레드를 잠시 멈춤
-            Thread.sleep(3000)
-            runOnUiThread{ // ui 관련 코드는 메인 스레드에서만 실행 되어야 함
-                showProgressBar(false)
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
             }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+        })
+
+        binding.rtbar1.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+//            ratingBar.rating = 3.5F
+            binding.tv1.text = "$rating"
         }
-    }
-    fun showProgressBar(show:Boolean){
-        if(show == true) { binding.pb1.visibility = View.VISIBLE }
-        else { binding.pb1.visibility = View.GONE }
+
     }
 }
