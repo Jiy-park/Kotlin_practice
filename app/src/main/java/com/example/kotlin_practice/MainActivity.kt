cc8bfd68a0894978317ee636fbaa5cfdeba3a356
Jiy-park <jiyong3954@gmail.com>
2023-01-05 17:11:35 +0900

    안드로이드(코틀린) 기본 연습 9 프래그먼트 기초

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -12,6 +12,7 @@ import android.widget.CompoundButton
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.ListFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlin_practice.databinding.ActivityMainBinding
@@ -23,43 +24,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        //스피너
-        val item = listOf<String>("select one", "1", "2", "3", "4")
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item)
-        binding.spinner.adapter = adapter
-        binding.spinner.onItemSelectedListener = object:AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                binding.tv1Main.text = item[position]
-            }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-
-        }
-        //리사이클러뷰
-        val data:MutableList<Memo> = createData()
-        var adapter2 = CustomAdapter()
-        adapter2.list_data = data
-
-        binding.recyclerView.adapter = adapter2
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)//세로 스크롤
-//        binding.recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)//가로 스크롤
-//        binding.recyclerView.layoutManager = GridLayoutManager(this,3)//그리드 두번째 인자는 한줄당 표현할 개수
+        setFragment()
     }
 
-    fun createData():MutableList<Memo>{
-        val data:MutableList<Memo> = mutableListOf()
-        for(i in 1..100){
-            val no:Int = i
-            val title:String = "${i}번째 텍스트"
-            val date:Long = System.currentTimeMillis()
-            data.add(Memo(no,title,date))
-        }
-        return data
+    fun setFragment(){
+        val listFragment:com.example.kotlin_practice.ListFragment = com.example.kotlin_practice.ListFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frameLayout, listFragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/kotlin_practice/Memo.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kotlin_practice
-
-data class Memo(var no:Int, var Title:String, var date:Long)
