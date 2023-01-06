e7473f9073ae95029360363275ee41fedd54ba61
Jiy-park <jiyong3954@gmail.com>
2023-01-06 15:22:39 +0900

    안드로이드(코틀린) 기본 연습 10-2 커스텀뷰 canvas.draw~~~ 사용

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -49,5 +49,10 @@ class CustomView(context:Context, text:String):View(context){
         //화면의 왼쪽 상단부터 (x:0, y:0)임
         //또한 drawText가 글자를 그릴 때 글자의 맨왼쪽 아래가 좌표의 시작점
         //textSize == 100F이므로 y좌표도 100F로 설정해야 글자가 안잘림
+
+        val paintCircle = Paint()
+        paintCircle.color = Color.BLUE
+        val circle = canvas?.drawCircle(100F,100F,50F,paintCircle)
+
     }
 }
