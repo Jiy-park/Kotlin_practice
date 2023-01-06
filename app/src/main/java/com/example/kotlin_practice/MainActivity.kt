bb697badcf249bdb4c999d008b20ef341157b707
Jiy-park <jiyong3954@gmail.com>
2023-01-06 17:07:43 +0900

    안드로이드(코틀린) 기본 연습 11 뷰페이저2/탭레이아웃 뷰페이저와 탭레이아웃 연동

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -16,18 +16,29 @@ import android.widget.CompoundButton
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.ListFragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kotlin_practice.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val fragmentList = listOf<Fragment>(FragmentA(),FragmentB(),FragmentC(),FragmentD())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("A", "B", "C", "D")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager){ tab, position->
+            tab.text = tabTitles[position]
+        }.attach()
     }
 }
