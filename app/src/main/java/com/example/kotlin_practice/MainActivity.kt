937fc0f5a0dcbdfd7ab2494a4494defb75a6306b
Jiy-park <jiyong3954@gmail.com>
2023-01-19 15:55:04 +0900

    안드로이드(코틀린) 기본 연습 16-2 스레드 루퍼와 핸들러를 이용한 간단한 타이머 루퍼 : MainActivity가 실행됨가 동시에 for문 하나가 무한 루프 돌고 있는 서브스레드라 생각하면 됨 핸들러 : 루퍼가 있는 메인스레드(MainActivity)에서 주로 사용되며 새로 생선된 스레드들과 메인 스레드와의 통신을 담당
    핸들러는 루퍼를 통해 전달되는 메시지를 받아서 처리하는 명령어 처리기로 사용
    메시지 : 루퍼의 큐에 값을 전달하기 위해 사용되는 클래스. 메시지 객체에 미리 정의해둔 코드를 입력하고 큐에 담아두면 루퍼가 꺼내서 핸들러에 전달

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -7,9 +7,8 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.net.Uri
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -36,57 +35,39 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
-    
-    /**
-     * 안드로이드  중요 규칙 "백그라운드 스레드는 UI구성 요소에 접근하면 안된다"
-     * EX) activity_main.xml에 텍스트뷰를 하나 만들고 백라운드 스레드를 통해 텍스트 뷰를 업데이트 하면 예외를 발생시키고 앱을 종료함
-     * 메인스레드 (UI스레드) 외에는 UI를 업데이트 할 수 없음. 이는 모든 프로그램 공통
-     * **/
-    
-    
+    var total = 0
+    var started = false
+
+    val handler = object :Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message) {
+            val minute = String.format("%02d",total/60)
+            val second = String.format("%02d",total%60)
+            binding.tvTimer.text = "$minute : $second"
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        var thread1 = WorkerThread() // Thread 객체 이용
-        thread1.start()
-
-        var thread2 = Thread(WorkerRunnable()) //Runnabel 인터페이스 이용
-        thread2.start()
-
-        Thread{ //람다식으로 Runnable 익명객체 이용
-            var i = 0
-            while(i < 10){
-                i++
-                Log.i("LOG_CHECK","lambda : "+i)
-            }
-        }.start()
-
-        thread(start = true){ //코틀린에서 제공하는 thread 구현 // 얘는 좀 다른데?
-            var i = 0
-            while(i < 10){
-                i++
-                Log.i("LOG_CHECK", "thread : "+i)
+        binding.btnStart.setOnClickListener {
+            started = true
+            thread(start = true){
+                while(started){
+                    Thread.sleep(1000)
+                    if(started == true){
+                        total++
+                        handler.sendEmptyMessage(0)
+                    }
+                }
             }
         }
-    }
-}
-class WorkerThread:Thread(){
-    override fun run() {
-        var i = 0
-        while(i < 10){
-            i++
-            Log.i("LOG_CHECK","thread : "+i)
-        }
-    }
-}
 
-class WorkerRunnable:Runnable{
-    override fun run() {
-        var i = 0
-        while(i < 10){
-            i++
-            Log.i("LOG_CHECK","runnable : "+i)
+        binding.btnEnd.setOnClickListener {
+            if(started == true){
+                started = false
+                total = 0
+                binding.tvTimer.text = "00 : 00"
+            }
         }
     }
 }
