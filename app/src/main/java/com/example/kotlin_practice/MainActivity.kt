bd7784c5b2dbf998b4b32f1affc611593faf8dc5
Jiy-park <jiyong3954@gmail.com>
2023-01-19 14:58:42 +0900

    안드로이드(코틀린) 기본 연습 15 카메라 및 갤러리 권환 획득 / 사용 카메라 권환과 갤러리 접근 권한 획득 후 사용

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -6,8 +6,10 @@ import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -15,7 +17,9 @@ import android.widget.ArrayAdapter
 import android.widget.CompoundButton
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.ListFragment
 import androidx.preference.PreferenceManager
@@ -25,34 +29,74 @@ import androidx.room.Room
 import com.example.kotlin_practice.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
 import com.google.android.material.tabs.TabLayoutMediator
+import java.io.File
+import java.util.jar.Manifest
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    var helper:RoomHelper? = null
+
+    lateinit var cameraPermission:ActivityResultLauncher<String> // 카메라 권한
+    lateinit var storagePermission:ActivityResultLauncher<String> // 저장소 권한
+    lateinit var cameraLauncher:ActivityResultLauncher<Uri> // 카메라 앱 호출
+    lateinit var galleryLauncher:ActivityResultLauncher<String> //갤러리
+    var photoUri:Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        //실제 프로젝트에선 allowMainThreadQueries 사용을 권장하지 않음 (room은 기본적으로 서브 스레드에서 동작하도록 설계되어 있음)
-        helper = Room.databaseBuilder(this, RoomHelper::class.java, "room_memo").allowMainThreadQueries().build()
+        Log.d("LOG_CHECK", "IN ON_CREATE1")
+//        setViews()
 
-        val adapter = RecyclerAdapter()
-        adapter.helper = helper
-        adapter.listData.addAll(helper?.roomMemoDao()?.getAll()?: listOf())
-        binding.recyclerMemo.adapter = adapter
-        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
-
-        binding.btnSave.setOnClickListener {
-            if(binding.editMemo.text.toString().isNotEmpty()){
-                val memo = RoomMemo(binding.editMemo.text.toString(),System.currentTimeMillis())
-                helper?.roomMemoDao()?.insert(memo)
-
-                adapter.listData.clear()
-                adapter.listData.addAll(helper?.roomMemoDao()?.getAll()?: listOf())
-                adapter.notifyDataSetChanged()
-                binding.editMemo.setText("")
+        storagePermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted->
+            Log.d("LOG_CHECK", "IN STORAGE_PERMISSION, isGranted : ${isGranted.toString()}")
+            if(isGranted == true) { setViews() }
+            else{
+                Toast.makeText(baseContext, "외부 저장소 권한을 승인해야 앱을 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                finish()
             }
         }
+
+        Log.d("LOG_CHECK", "IN ON_CREATE2")
+        cameraPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
+            Log.d("LOG_CHECK", "IN CAMERA_PERMISSION")
+            if(isGranted == true) { openCamera() }
+            else {
+                Toast.makeText(baseContext, "카메라 권한을 승인해야 카메라를 사용할 수 있습니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+
+        }
+        Log.d("LOG_CHECK", "IN ON_CREATE3")
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.TakePicture()){ isSuccess->
+            Log.d("LOG_CHECK", "IN CAMERA_LAUNCHER")
+            if(isSuccess == true) { binding.ivPrev.setImageURI(photoUri) }
+        }
+        galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){ uri->
+            binding.ivPrev.setImageURI(uri)
+        }
+        storagePermission.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    }
+
+    fun setViews(){
+        Log.d("LOG_CHECK", "IN SET_VIEWS")
+        binding.btnCamera.setOnClickListener {
+            cameraPermission.launch(android.Manifest.permission.CAMERA)
+        }
+        binding.btnGallery.setOnClickListener {
+            openGallery()
+        }
+    }
+
+    fun openCamera(){
+        Log.d("LOG_CHECK", "IN OPEN_CAMERA")
+        val photoFile = File.createTempFile("IMG_", ".jpg", getExternalFilesDir(Environment.DIRECTORY_PICTURES))
+        photoUri = FileProvider.getUriForFile(this, "${packageName}.provider", photoFile)
+        cameraLauncher.launch(photoUri)
+    }
+
+    fun openGallery(){
+        galleryLauncher.launch("image/*")
     }
 }
