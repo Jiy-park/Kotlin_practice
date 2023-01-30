22141d214cb5fb443f37dcd1963cc2969b1b0f9a
Jiy-park <jiyong3954@gmail.com>
2023-01-30 21:22:33 +0900

    안드로이드(코틀린) 기본 연습 18-2 구글맵 사용하기2 구글맵 호출 및 원하는 위도, 경도에 마커 찍기, 커스텀 마커 사용

--- app/src/main/java/com/example/kotlin_practice/MapsActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MapsActivity.kt
@@ -1,5 +1,8 @@
 package com.example.kotlin_practice
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,6 +13,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.kotlin_practice.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -31,10 +35,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        val bitmapDrawable:BitmapDrawable // 마커 이미지 변경 ** 롤리팝 버전 이하는 resources로 변경해야 함
+
+
+        bitmapDrawable = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) getDrawable(R.drawable.marker) as BitmapDrawable //이거 marker.png 사이즈가 너무 큼
+                            else resources.getDrawable(R.drawable.marker) as BitmapDrawable
+
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmapDrawable.bitmap, 50, 50, false) //비트맵 스케일링
+
+        val discriptor = BitmapDescriptorFactory.fromBitmap(scaledBitmap)
+
+
         val LATLNG = LatLng(37.820981, 127.100627) //위도 경도
-        val camera = CameraPosition.builder().target(LATLNG).zoom(15.0F).build()
+        val camera = CameraPosition.builder()
+            .target(LATLNG).zoom(15.0F)
+            .build()
         val cameraUpdate = CameraUpdateFactory.newCameraPosition(camera)
         mMap.moveCamera(cameraUpdate)
 
+
+        val markerOptions = MarkerOptions()
+            .position(LATLNG)
+            .icon(discriptor)
+            .title("MY HOME")
+            .snippet("37.820981, 127.100627")//이건 하나만 적용되는듯?
+        mMap.addMarker(markerOptions)
     }
 }
