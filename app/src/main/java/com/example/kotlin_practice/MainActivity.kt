bd8960f77905e4d8cfe6baeddac978c2b099f21f
Jiy-park <jiyong3954@gmail.com>
2023-02-06 17:19:12 +0900

    안드로이드(코틀린) 기본 연습 21-6 파이어베이스 스토리지 파이어베이스 스토리지를 이용한 이미지 업로드 및 다운로드 실제 서비스에선 데이터베이스 필드에 경로를 저장해 놓고 사용한다고 함

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -1,117 +1,56 @@
 package com.example.kotlin_practice
 
-import android.app.Activity
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.net.Uri
 import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.CompoundButton
-import android.widget.SeekBar
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.FileProvider
-import androidx.core.view.PointerIconCompat.load
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.ListFragment
-import androidx.preference.PreferenceManager
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.room.Room
 import com.bumptech.glide.Glide
-import com.bumptech.glide.Glide.with
 import com.example.kotlin_practice.databinding.ActivityMainBinding
-import com.example.kotlin_practice.model.User
-import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
-import com.google.android.material.tabs.TabLayoutMediator
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.*
-import retrofit2.*
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import java.util.jar.Manifest
-import kotlin.concurrent.thread
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    val database =  Firebase.database("https://android-with-kotlin-8d5bc-default-rtdb.asia-southeast1.firebasedatabase.app/")
-    val usersRef = database.getReference("users")
+    val storage = Firebase.storage("gs://android-with-kotlin-8d5bc.appspot.com")
+    val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){ uri->
+        uri?.let { uploadImage(it) }
+    }
+    val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted->
+        if(isGranted) { galleryLauncher.launch("image/*") }
+        else { Toast.makeText(this@MainActivity, "외부 저장소 읽기 권한을 승인해야 사용할 수 있습니다.", Toast.LENGTH_SHORT).show() }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        with(binding){
-            btnSignin.setOnClickListener { signin() }
-            btnSignup.setOnClickListener { signup() }
-        }
+        binding.btnUpload.setOnClickListener { permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE) }
+        binding.btnDownload.setOnClickListener { downloadImage("images/temp_1675670937747.jpeg") }
     }
 
-    fun signup(){
-        with(binding){
-            val id = edID.text.toString()
-            val password = edPassword.text.toString()
-            val name = edName.text.toString()
+    fun uploadImage(uri: Uri){
+        val fullPath = makeFilePath("images", "temp", uri)
+        val imageRef = storage.getReference(fullPath)
+        val uploadTask = imageRef.putFile(uri)
 
-            if(id.isNotEmpty() && password.isNotEmpty() && name.isNotEmpty()){
-                usersRef.child(id).get().addOnSuccessListener {
-                    if(it.exists()) { Toast.makeText(this@MainActivity, "아이디가 존재합니다.", Toast.LENGTH_SHORT).show() }
-                    else{
-                        val user = User(id, password, name)
-                        usersRef.child(id).setValue(user)
-                        signin()
-                    }
-                }
+        uploadTask
+            .addOnFailureListener { Log.d("LOG_CHECK", "MainActivity :: uploadImage() called :: 실패 : ${it.message}") }
+            .addOnSuccessListener { Log.d("LOG_CHECK", "MainActivity :: uploadImage() called :: 성공 : $fullPath") }
+    }
+
+    fun makeFilePath(path: String, userId: String, uri: Uri): String {
+        val mimeType = contentResolver.getType(uri) ?: "/none" //마임타입 ex) images/jpeg
+        val ext = mimeType.split("/")[1] //확장자 ex) jpeg
+        val timeSuffix = System.currentTimeMillis() // 시간값 ex)123123123
+        return "${path}/${userId}_${timeSuffix}.$ext" // 파일 경로
+    }
+
+    fun downloadImage(path:String){
+        storage.getReference(path).downloadUrl
+            .addOnSuccessListener { uri->
+                Glide.with(this@MainActivity).load(uri).into(binding.iv)
             }
-            else { Toast.makeText(this@MainActivity, "아이디 비밀번호 별명을 모두 입력해야 합니다.", Toast.LENGTH_SHORT).show() }
-        }
-    }
-
-    fun signin(){
-        with(binding){
-            val id = edID.text.toString()
-            val password = edPassword.text.toString()
-
-            if(id.isNotEmpty() && password.isNotEmpty()){
-                usersRef.child(id).get().addOnSuccessListener {
-                    if(it.exists()){
-                        it.getValue(User::class.java)?.let { user->
-                            if(user.password == password) { goChatRoomList(user.id, user.name) }
-                            else { Toast.makeText(this@MainActivity, "비밀번호가 다릅니다.", Toast.LENGTH_SHORT).show() }
-                        }
-                    }
-                    else { Toast.makeText(this@MainActivity, "아이디가 존재하지 않습니다.",Toast.LENGTH_SHORT).show() }
-                }
-            }
-            else { Toast.makeText(this@MainActivity, "아이디 비밀번호를 입력해야 합니다.",Toast.LENGTH_SHORT).show() }
-        }
-    }
-
-    fun goChatRoomList(userId:String, userName:String){
-        val intent = Intent(this@MainActivity, ChatListActivity::class.java)
-
-        intent.putExtra("userId", userId)
-        intent.putExtra("userName", userName)
-        startActivity(intent)
+            .addOnFailureListener { Log.d("LOG_CHECK", "MainActivity :: downloadImage() called :: 실패 :${it.message}") }
     }
 }
