04269c0996ccd4025df6aede2ecd95736af17670
Jiy-park <jiyong3954@gmail.com>
2023-02-06 17:52:52 +0900

    안드로이드(코틀린) 기본 연습 21-7 파이어베이스 푸쉬메시지 파이어베이스 메시징을 이용한 푸쉬메시지 보내기

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -8,49 +8,26 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.example.kotlin_practice.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    val storage = Firebase.storage("gs://android-with-kotlin-8d5bc.appspot.com")
-    val galleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()){ uri->
-        uri?.let { uploadImage(it) }
-    }
-    val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted->
-        if(isGranted) { galleryLauncher.launch("image/*") }
-        else { Toast.makeText(this@MainActivity, "외부 저장소 읽기 권한을 승인해야 사용할 수 있습니다.", Toast.LENGTH_SHORT).show() }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.btnUpload.setOnClickListener { permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE) }
-        binding.btnDownload.setOnClickListener { downloadImage("images/temp_1675670937747.jpeg") }
-    }
-
-    fun uploadImage(uri: Uri){
-        val fullPath = makeFilePath("images", "temp", uri)
-        val imageRef = storage.getReference(fullPath)
-        val uploadTask = imageRef.putFile(uri)
-
-        uploadTask
-            .addOnFailureListener { Log.d("LOG_CHECK", "MainActivity :: uploadImage() called :: 실패 : ${it.message}") }
-            .addOnSuccessListener { Log.d("LOG_CHECK", "MainActivity :: uploadImage() called :: 성공 : $fullPath") }
-    }
-
-    fun makeFilePath(path: String, userId: String, uri: Uri): String {
-        val mimeType = contentResolver.getType(uri) ?: "/none" //마임타입 ex) images/jpeg
-        val ext = mimeType.split("/")[1] //확장자 ex) jpeg
-        val timeSuffix = System.currentTimeMillis() // 시간값 ex)123123123
-        return "${path}/${userId}_${timeSuffix}.$ext" // 파일 경로
-    }
-
-    fun downloadImage(path:String){
-        storage.getReference(path).downloadUrl
-            .addOnSuccessListener { uri->
-                Glide.with(this@MainActivity).load(uri).into(binding.iv)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task->
+            if(task.isSuccessful == false){
+                Log.e("토큰", "Fetching FCM registration token failed",task.exception)
+                return@OnCompleteListener
             }
-            .addOnFailureListener { Log.d("LOG_CHECK", "MainActivity :: downloadImage() called :: 실패 :${it.message}") }
+            val token = task.result
+            Log.d("토큰", "재호출 = ${token.toString()}")
+        })
+
     }
+
+
 }
