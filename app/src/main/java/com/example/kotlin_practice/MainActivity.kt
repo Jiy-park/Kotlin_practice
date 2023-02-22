28c022ba4f437f363a97e47908dfb384939aca56
Jiy-park <jiyong3954@gmail.com>
2023-02-22 22:34:11 +0900

    안드로이드(코틀린) 기본 연습 22-2 라이브데이터 + 데이터바인딩 데이터 바인딩을 이용하면 더 깔끔한 코드가 나옴 참고 :: https://hanyeop.tistory.com/168

--- app/src/main/java/com/example/kotlin_practice/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_practice/MainActivity.kt
@@ -2,32 +2,29 @@ package com.example.kotlin_practice;
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.kotlin_practice.databinding.ActivityMainBinding
-import com.example.livedata.MyViewModel
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var binding:ActivityMainBinding
     lateinit var myViewModel: MyViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
-        var cnt = 0
-
-        binding.btn1.setOnClickListener {
-            myViewModel.plus()
-        }
-
-        binding.btn2.setOnClickListener {
-            cnt++
-            binding.tv2.text = cnt.toString()
-        }
-
+//        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this@MainActivity,R.layout.activity_main)
         myViewModel = ViewModelProvider(this@MainActivity)[MyViewModel::class.java]
-        myViewModel.value.observe(this@MainActivity, Observer {
-            binding.tv1.text = it.toString()
-        })
+        binding.myViewModel = myViewModel
+        binding.lifecycleOwner = this@MainActivity
+
+//        binding.btn1.setOnClickListener {
+//            myViewModel.plus()
+//        }
+
+//        myViewModel.value.observe(this@MainActivity, Observer {
+//            binding.tv1.text = it.toString()
+//        })
     }
 }
--- app/src/main/java/com/example/kotlin_practice/MyViewModel.kt
+++ app/src/main/java/com/example/kotlin_practice/MyViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.livedata
+package com.example.kotlin_practice;
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -6,15 +6,23 @@ import androidx.lifecycle.ViewModel
 
 class MyViewModel():ViewModel() {
     private val _value = MutableLiveData<Int>()
+    private val _value2 = MutableLiveData<Int>()
 
     val value:LiveData<Int>
         get() = _value
+    val value2:LiveData<Int>
+        get() = _value2
+
 
     init {
         _value.value = 0
+        _value2.value = 0
     }
 
     fun plus(){
         _value.value = _value.value?.plus(1)
+    }
+    fun setValue(){
+        _value2.value = _value.value
     }
 }
